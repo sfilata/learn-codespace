e0dd5c875969e77847ecedd3219623a105a31af9
Ezio Auditore <12068103+sfilata@users.noreply.github.com>
2023-10-26 03:19:10 +0000

    feat: add the home page and update the social media.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Sfilata",
+  title: "FE Programmer and IDE Developer",
+  email: "sfilata@hotmail.com",
+  gitHub: "sfilata",
+  instagram: "",
+  linkedIn: "sfilata",
+  medium: "shubaiqiao",
+  twitter: "Sfilata1",
+  youTube: "sfilata",
 };
 
 const primaryColor = "#4E567E";
